5270160afaaa54659c953546265b99e792d7cf80
Natnael Asfaw <149261198+Natthy2023@users.noreply.github.com>
2026-03-22 06:31:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         // FIXED: Converted manualChunks from Object to Function
